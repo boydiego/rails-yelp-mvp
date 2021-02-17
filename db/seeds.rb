cd481747c4625acdb9b692adc4e732dce61fa1f6
diego boy <diegolive5000@gmail.com>
2021-02-17 15:39:45 +0100

    models for restaurant and review

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,16 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+require 'faker'
+puts "Cleaning database..."
+Restaurant.destroy_all
+puts "Creating restaurants..."
+sample_category = ["chinese", "italian", "japanese", "french", "belgian"]
+10.times do
+  restaurant = Restaurant.create!(name: Faker::Restaurant.name, address: Faker::Address.street_address, phone_number: Faker::PhoneNumber.cell_phone_in_e164, category: sample_category.sample)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
+
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
